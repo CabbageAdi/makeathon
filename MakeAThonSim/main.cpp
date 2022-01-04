67bd53053f2868b6fdb576eb352c5d327a12705b
Adi Mathur <65034167+IDoEverything@users.noreply.github.com>
2022-01-04 16:59:53 +0530

    sim random line cleanup

--- MakeAThonSim/main.cpp
+++ MakeAThonSim/main.cpp
@@ -48,7 +48,7 @@ bool doIntersect(Vector2 p1, Vector2 q1, Vector2 p2, Vector2 q2);
 //maze values
 #define MAZE_SIZE 22
 #define MAZE_THICKNESS 2
-#define POINTS 43
+#define POINTS 35
 float mazePoints[POINTS][2][2] = {
         {{0, 0}, {0, 8}},
         {{0, 0}, {3, 0}},
@@ -128,9 +128,6 @@ void UpdateDrawFrame(void) {
 
     BeginDrawing();
     ClearBackground(GREEN);
-
-    //2d elements
-
 
     //robot
     Vector3 pos = {x, 0, y};
@@ -232,9 +229,6 @@ void UpdateDrawFrame(void) {
     Vector2 topCorner1 = {x + deg_sin(biggerAngle1) * biggerHypotenuse, y + deg_cos(biggerAngle1) * biggerHypotenuse};
     float biggerAngle2 = 90 - (rotation + biggerSubAngle);
     Vector2 topCorner2 = {x + deg_cos(biggerAngle2) * biggerHypotenuse, y + deg_sin(biggerAngle2) * biggerHypotenuse};
-
-    //DrawMarker(topCorner1);
-    //DrawMarker(topCorner2);
 
     float smallerHypotenuse = -sqrt(ssxdist * ssxdist + backdist * backdist);
     float smallerSubAngle = asin(ssxdist / smallerHypotenuse) * 180 / PI;
@@ -347,11 +341,9 @@ void UpdateDrawFrame(void) {
         rotationSpeed = maxRotSpeed;
     }
 
-    //Log("left: " + to_string(leftMin) + ", right: " + to_string(rightMin) + ", forward: " + to_string(forwardMin));
-    //Log("x: " + to_string(x) + ", y: " + to_string(y));
-
     EndMode3D();
 
+    //text
     DrawText("RoboKnights Make-a-thon", 5, 6, 14, BLACK);
     DrawText("Made with <3 by Adi Mathur", 5, 22, 10, GRAY);
 
@@ -384,9 +376,6 @@ float GetDistance(Vector2 start, Vector2 end, Vector2 sensorPos, float rot){
 }
 
 // https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
-
-// Given three collinear points p, q, r, the function checks if
-// point q lies on line segment 'pr'
 bool onSegment(Vector2 p, Vector2 q, Vector2 r)
 {
     if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
@@ -395,53 +384,28 @@ bool onSegment(Vector2 p, Vector2 q, Vector2 r)
 
     return false;
 }
-
-// To find orientation of ordered triplet (p, q, r).
-// The function returns following values
-// 0 --> p, q and r are collinear
-// 1 --> Clockwise
-// 2 --> Counterclockwise
 int orientation(Vector2 p, Vector2 q, Vector2 r)
 {
-    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
-    // for details of below formula.
     int val = (q.y - p.y) * (r.x - q.x) -
               (q.x - p.x) * (r.y - q.y);
 
-    if (val == 0) return 0;  // collinear
+    if (val == 0) return 0;
 
-    return (val > 0)? 1: 2; // clock or counterclock wise
+    return (val > 0)? 1: 2;
 }
-
-// The main function that returns true if line segment 'p1q1'
-// and 'p2q2' intersect.
 bool doIntersect(Vector2 p1, Vector2 q1, Vector2 p2, Vector2 q2)
 {
-    // Find the four orientations needed for general and
-    // special cases
     int o1 = orientation(p1, q1, p2);
     int o2 = orientation(p1, q1, q2);
     int o3 = orientation(p2, q2, p1);
     int o4 = orientation(p2, q2, q1);
-
-    // General case
     if (o1 != o2 && o3 != o4)
         return true;
-
-    // Special Cases
-    // p1, q1 and p2 are collinear and p2 lies on segment p1q1
     if (o1 == 0 && onSegment(p1, p2, q1)) return true;
-
-    // p1, q1 and q2 are collinear and q2 lies on segment p1q1
     if (o2 == 0 && onSegment(p1, q2, q1)) return true;
-
-    // p2, q2 and p1 are collinear and p1 lies on segment p2q2
     if (o3 == 0 && onSegment(p2, p1, q2)) return true;
-
-    // p2, q2 and q1 are collinear and q1 lies on segment p2q2
     if (o4 == 0 && onSegment(p2, q1, q2)) return true;
-
-    return false; // Doesn't fall in any of the above cases
+    return false;
 }
 
 float deg_sin(float angle){
